9ca8a7f69932c44e1f75fc638707b5cabf7d0bab
MrCrabs <MrCrabs67forms@outlook.com>
2024-05-12 15:29:33 +0300

    filter initiate

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,12 +5,14 @@ import Content from '../../components/Content';
 import Footer from '../../components/Footer';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProductsIds } from '@store/products/thunks';
+import { getPass } from '../../config';
 
 const Home = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchProductsIds()); // вызов один раз
+        console.log('pass = ', getPass());
         return () => {
             // код при размонтировании компонента
         };
